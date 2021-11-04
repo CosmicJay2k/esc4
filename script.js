07584c1fcd4b662a25e6e7e37edc3c317f6bbd72
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2021-11-04 12:34:03 +0100

    Added closing functionality for clicking outside mobile menu popup

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+const modal = document.querySelector(".modal");
+
 openMobMenu.addEventListener("click", function () {
-  document.querySelector(".menuBox").style.display = "flex";
-  document.querySelector(".container").style.opacity = "50%";
+  modal.style.display = "block";
+  /*document.querySelector(".container").style.opacity = "50%";*/
 })
 
 closeMobMenu.addEventListener("click", function () {
-  document.querySelector(".menuBox").style.display = "none";
-  document.querySelector(".container").style.opacity = "100%";
+  modal.style.display = "none";
+  /*document.querySelector(".container").style.opacity = "100%";*/
 })
+
+window.onclick = function (e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
