8d0e6c2d4be4308737e27e864cb714bb7a9b2d7c
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2021-11-05 07:58:52 +0100

    Removed old code regarding opacity, moved to animation

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ const modal = document.querySelector(".modal");
 
 openMobMenu.addEventListener("click", function () {
   modal.style.display = "block";
-  /*document.querySelector(".container").style.opacity = "50%";*/
 })
 
 closeMobMenu.addEventListener("click", function () {
   modal.style.display = "none";
-  /*document.querySelector(".container").style.opacity = "100%";*/
 })
 
 window.onclick = function (e) {
