e11da7427201777b01efc50bf6db21f1f8c2e5bb
John Tannenberg <tannenbergster@gmail.com>
2021-12-01 22:35:18 +0100

    Add image from API to card

--- src/javascript/render.js
+++ src/javascript/render.js
@@ -5,21 +5,26 @@ export default function render(data) {
   newItem.setAttribute('class', 'popCard');
 
   newItem.innerHTML = `
-    <h2 class="title">${data.title} (${data.type})</h2>
-    <div>
-      <div>
-        <p class="stars"></p>
-        <p class="stars"></p>
-        <p class="stars"></p>
-        <p class="stars"></p>
-        <p class="stars"></p>
-        <p>${data.rating}</p>
-      </div>
-      <p class="players">${data.minParticipants} - ${data.maxParticipants} participants</p>
+    <div class="img-container">
+      <img src="${data.image}"/>
     </div>
-    <p class="description">${data.description}</p>
-    <div class="book">
-      <a href="#">Book this room</a>
+    <div class="text-container">
+      <h2 class="title">${data.title} (${data.type})</h2>
+      <div>
+        <div>
+          <p class="stars"></p>
+          <p class="stars"></p>
+          <p class="stars"></p>
+          <p class="stars"></p>
+          <p class="stars"></p>
+          <p>${data.rating}</p>
+        </div>
+        <p class="players">${data.minParticipants} - ${data.maxParticipants} participants</p>
+      </div>
+      <p class="description">${data.description}</p>
+      <div class="book">
+        <a href="#">Book this room</a>
+      </div>
     </div>
   `
 
