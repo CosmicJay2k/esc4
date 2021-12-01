160a69d5177a093779911892fb3a02c3237e4657
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2021-12-01 09:56:32 +0100

    Add function for troubleshooting menu change when switching from mobile to desktop and back

--- src/javascript/mobMenu.js
+++ src/javascript/mobMenu.js
@@ -2,22 +2,27 @@ export default function openMobileMenu() {
   const modal = document.querySelector(".modal");
 
   openMobMenu.addEventListener("click", function () {
-    document.querySelector('.menu').setAttribute('class', 'mobMenu');
+    document.querySelector('#menuList').setAttribute('class', 'mobMenu');
     modal.style.display = "block";
     document.querySelector('.mobMenuIcon').style.display = 'none';
   })
 
   closeMobMenu.addEventListener("click", function () {
     modal.removeAttribute('style');
-    document.querySelector('.mobMenu').setAttribute('class', 'menu');
     document.querySelector('.mobMenuIcon').removeAttribute('style');
   })
 
   window.onclick = function (e) {
     if (e.target == modal) {
       modal.removeAttribute('style');
-      document.querySelector('.mobMenu').setAttribute('class', 'menu');
       document.querySelector('.mobMenuIcon').removeAttribute('style');
     }
+  }
+
+  window.onresize = function () {
+    if (window.innerWidth > 768)
+      document.querySelector('#menuList').setAttribute('class', 'menu');
+    else
+      document.querySelector('#menuList').setAttribute('class', 'mobMenu');
   }
 };
