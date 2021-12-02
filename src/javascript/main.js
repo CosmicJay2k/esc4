d1deb304bf7d737859fd75e93f75ea597dbdc17d
John Tannenberg <tannenbergster@gmail.com>
2021-12-02 14:25:53 +0100

    Add if statements to only run relevant drawing of challenges

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,8 +1,15 @@
-import { topThree } from "./drawChallenges.js";
+import { allChallenges, topThree } from "./drawChallenges.js";
 import fetchData from "./fetchData.js";
 import openMobileMenu from "./mobMenu.js";
 
 const data = await fetchData();
 
 openMobileMenu();
-topThree(data);
+
+if (document.querySelector('.index.html')){
+  topThree(data);
+}
+
+if (document.querySelector('.challenges.html')){
+  allChallenges(data);
+}
